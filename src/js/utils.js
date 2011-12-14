b42d1a56d185c7645ac770135368dd6f2223538d
Ivan Mashintsev <mashintsev@gmail.com>
2011-12-14 17:18:30 +0400

    Fix #124

--- src/js/utils.js
+++ src/js/utils.js
@@ -575,7 +575,7 @@ var getOwnerViewConfig = function(view) {
 		storeLimit: view.isSetView ? view.setViewStore.currentPage * view.setViewStore.pageSize : undefined,
 		storePage: view.isSetView && view.setViewStore.currentPage,
 		lastSelectedRecord: view.lastSelectedRecord,
-		scrollOffset: view.form.scroller.getOffet()
+		scrollOffset: view.form.scroller.getOffset()
 	}};
 };
 

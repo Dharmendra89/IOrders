aac1e023622fbe6d28661a73a70c7362410a3d8e
Ivan Mashintsev <mashintsev@gmail.com>
2011-12-27 12:25:00 +0400

    Closes #134 Титульные поля

--- src/js/ux/PagingSelectField.js
+++ src/js/ux/PagingSelectField.js
@@ -1,7 +1,7 @@
 var PagingSelectField = Ext.extend(Ext.form.Select, {
 
 	getListPanel: function() {
-	    if (!this.listPanel) {
+/*	    if (!this.listPanel) {
 	        this.listPanel = new Ext.Panel({
 	            floating         : true,
 	            stopMaskTapEvent : false,
@@ -26,6 +26,24 @@ var PagingSelectField = Ext.extend(Ext.form.Select, {
             });
 	    }
         return this.listPanel;
-    }
+        */
+		var panel = PagingSelectField.superclass.getListPanel.apply(this, arguments),
+			list = panel.getComponent('list'),
+			table = Ext.getStore('tables').getById(this.store.model.prototype.modelName),
+			titleColumns = table.getTitleColumns()
+		;
+
+		list.itemTpl = ['<div class="x-list-label">{' + this.displayField + '}</div>'];
+		titleColumns.each(function(col) {list.itemTpl.push('<div>{' + col.get('name') + '}</div>');});
+		list.itemTpl.push('<span class="x-list-selected"></span>');
+
+		list.tpl = '<tpl for="."><div class="x-list-item ' + list.itemCls + '"><div class="x-list-item-body">' + list.itemTpl.join('') + '</div>';
+		list.tpl += '</div></tpl>';
+		list.tpl = new Ext.XTemplate(list.tpl);
+
+		list.initPlugin(new Ext.plugins.ListPagingPlugin({autoPaging: true}));
+
+		return panel;
+	}
 });
 Ext.reg('pagingselectfield', PagingSelectField);
